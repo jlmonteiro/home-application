7bba6e26c24e5d12e7da87f23c88398396994b7d
Jorge Luiz Monteiro <jorgemonteiro@gmail.com>
2026-04-16 13:35:56 -0300

    ci(docs): configure github workflows for build and deployment
    - Add Deploy Documentation workflow to publish to GitHub Pages on push to main
    - Add Documentation CI workflow to validate doc builds on Pull Requests
    - Update mkdocs.yml with site_url for correct link generation
    - Update local generate-docs.sh with --strict flag for parity with CI

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -2,6 +2,6 @@
 set -e
 cd "$(dirname "$0")/.."
 
-docker run --rm -v "$PWD:/docs" -u "$(id -u):$(id -g)" squidfunk/mkdocs-material build
+docker run --rm -v "$PWD:/docs" -u "$(id -u):$(id -g)" squidfunk/mkdocs-material build --strict
 
 echo "Docs built to build/site/"
